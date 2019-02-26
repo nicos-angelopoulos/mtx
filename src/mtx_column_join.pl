46a21a1c99310c29594172d5fd40930b054f6a6a
Nicos Angelopoulos <nicos@stoics.org.uk>
2019-02-26 11:33:54 +0000

    OPTS:   new value first for is_unique(IsUnique) FIXED:  do not consume matching lines if we are in non exhaustive join (is_exhaustive(false))

--- src/mtx_column_join.pl
+++ src/mtx_column_join.pl
@@ -16,7 +16,7 @@ position and
 
 Opts
   * add_columns([])
-    which columns to add (all exept ClmBase/ClmMatch by default)
+    which columns to add (by default all but ClmBase and ClmMatch)
 
   * at(At=[])
     if an integer, additional columns are added from that position onwards.
@@ -24,10 +24,13 @@ Opts
     the empty list which is a token for adding the columns at the end.
 
   * is_exhaustive(IsExh=false)
-    should we check that all rows of MtxMatch were used ?
+    should we check that all rows of MtxMatch were used ? 
+    if false matched lines are not "consumed"
 
   * is_unique(IsUnique=true)
-    should we check that only a single row matches
+    should we check that only a single row matches, _true_ throws a ball, 
+    _false_ creates a new row for each matching row and first selects the first matching one
+    (debug(mtx(column_join(multi)) triggers the printing of discarded matching rows))
 
   * match_column(ClmMatch)
     column id of MtxExt if different that ClmBase
@@ -48,13 +51,14 @@ mtx_column_join( MtxB, ClmB, MtxM, MtxOut, Args ) :-
     mtx( MtxB, [HdrB|RowsB] ),
     mtx( MtxM, [HdrM|RowsM] ),
     options( at(AtOpt), Opts ),
-    mtx_column_join_at_list( AtOpt, HdrB, HdrM, Ats ),
     ( memberchk(match_column(ClmM),Opts) -> true; ClmM = ClmB ),
     mtx_header_column_name_pos( HdrM, ClmM, _CnmM, PosM ),
-    mtx_header_column_name_pos( HdrM, ClmB, _CnmB, PosB ),
+    mtx_header_column_name_pos( HdrB, ClmB, _CnmB, PosB ),
     options( add_columns(AddCidsIn), Opts ),
     mtx_column_join_add_columns( AddCidsIn, HdrM, PosM, MIdcs ),
+    mtx_column_join_at_list( AtOpt, HdrB, MIdcs, Ats ),
     options( [is_unique(IsUnq),is_exhaustive(IsExh)], Opts ),
+    write( midcs(MIdcs) ), nl,
     mtx_column_join_rows( RowsB, PosB, RowsM, PosM, MIdcs, Ats, IsUnq, IsExh, RowsOut ),
     HdrB =.. [Rn|RbArgs],
     mtx_column_join_rows_add_1( [HdrM], Rn, RbArgs, MIdcs, Ats, [HdrOut], _ ),
@@ -72,15 +76,27 @@ mtx_column_join_rows( [], _PosB, RowsM, _PosM, _MIdcs, _Ats, _IsUnq, IsExh, [] )
 mtx_column_join_rows( [Rb|Rbs], PosB, RowsM, PosM, MIdcs, Ats, IsUnq, IsExh, RowsOut ) :-
    arg( PosB, Rb, JoinVal ),
    partition( at_arg(PosM,JoinVal), RowsM, JoinRowsM, RemRowsM ),
+   length( JoinRowsM, MatchLen ),
+   debug( mtx(column_join), 'Doing join value: ~w, match length: ~d', [JoinVal,MatchLen] ),
    mtx_column_join_rows_add( IsUnq, JoinRowsM, JoinVal, Rb, MIdcs, Ats, RowsOut, TRowsOut ),
-   mtx_column_join_rows( Rbs, PosB, RemRowsM, PosM, MIdcs, Ats, IsUnq, IsExh, TRowsOut ).
+   ( IsExh == true -> NextRowsM = RemRowsM ; NextRowsM = RowsM ),
+   mtx_column_join_rows( Rbs, PosB, NextRowsM, PosM, MIdcs, Ats, IsUnq, IsExh, TRowsOut ).
 
 mtx_column_join_rows_add( true, [_A,_B|C], JoinVal, _Rb, _MIdcs, _Ats, _RowsOut, _TRowsOut ) :-
-    !,
     length( C, LenC ),
     JoinMLen is LenC + 2,
     throw( mtx_column_join(non_unique_rows_match(JoinVal,JoinMLen)) ). % fixme: error
-mtx_column_join_rows_add( _, JoinMs, _JoinVal, Rb, MIdcs, Ats, RowsOut, TRowsOut ) :-
+mtx_column_join_rows_add( first, [First|TMatch], JoinVal, Rb, MIdcs, Ats, RowsOut, TRowsOut ) :-
+    Rb =.. [Rn|RbArgs],
+    ( TMatch == [] -> true
+                    ; 
+                    Mess = 'For join value: ~w, ignoring match row: ~w',
+                    findall( _, ( member(Row,TMatch),
+                                  debug(mtx(column_join(multi)),Mess,[JoinVal,Row])
+                                ), _)
+    ),
+    mtx_column_join_rows_add_1( [First], Rn, RbArgs, MIdcs, Ats, RowsOut, TRowsOut ).
+mtx_column_join_rows_add( false, JoinMs, _JoinVal, Rb, MIdcs, Ats, RowsOut, TRowsOut ) :-
     Rb =.. [Rn|RbArgs],
     mtx_column_join_rows_add_1( JoinMs, Rn, RbArgs, MIdcs, Ats, RowsOut, TRowsOut ).
 
@@ -109,10 +125,11 @@ mtx_column_join_add_columns( AddCidsIn, HdrM, PosM, CIdcs ) :-
                 numlist( 1, MArity, AllPossM ),
                 nth1( PosM, AllPossM, _, CIdcs )
                 ; 
-                mtx_header_cids_order( AddCidsIn, CIdcs )
+                mtx_header_cids_order( HdrM, AddCidsIn, CIdcs )
     ).
 
-mtx_column_join_at_list( AtOpt, HdrB, HdrM, At ) :-
+% mtx_column_join_at_list( AtOpt, HdrB, %HdrM, Idcs, At ) :-
+mtx_column_join_at_list( AtOpt, HdrB, Idcs, At ) :-
     \+ var(AtOpt), % fixme: error
     ( AtOpt == [] ->
         functor( HdrB, _, ArityB ),
@@ -124,9 +141,10 @@ mtx_column_join_at_list( AtOpt, HdrB, HdrM, At ) :-
         At = AtIn
         ;
         ( integer(AtIn) ->
-            functor( HdrM, _, ArityM ),
-            MLim is ArityM - 1,
-            findall( AnAt, (between(1,MLim,I), AnAt is I + AtIn - 1), At )
+            % functor( HdrM, _, ArityM ),
+            % MLim is ArityM - 1,
+            length( Idcs, Len ),
+            findall( AnAt, (between(1,Len,I), AnAt is I + AtIn - 1), At )
             ;
             throw( unknown_type_for_at(AtIn) )  % fixme: pretty print
         )
