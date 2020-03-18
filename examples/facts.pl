e0e2ad2d957b0189064bd75b72c118574cbc1e46
Nicos Angelopoulos <nicos@gmx.co.uk>
2020-03-18 19:36:56 +0000

    EG:     debug matrices in memory.

--- examples/facts.pl
+++ examples/facts.pl
@@ -8,5 +8,7 @@ facts :-
     mtx_facts( data('mtcars.csv'), Mtcars ),
     debug( mtx(facts), 'Mtcars: ~w', [Mtcars] ),
     debug( mtx(facts), 'Listing of module: ~w', [Mtcars] ),
-    listing( Mtcars:_ ).
-
+    listing( Mtcars:_ ),
+    mtx_matrices_in_memory( InMem ),
+    nl,
+    debug( mtx(facts), 'In memory: ~w', [InMem] ).
