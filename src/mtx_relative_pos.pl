4b7dbc511c002d93a7f7a52bcfb8e3c80d03c2b9
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-02 19:53:44 +0100

    FIXED:  mtx_relative_pos/5 doc example of /5 version and an extra new example for /4

--- src/mtx_relative_pos.pl
+++ src/mtx_relative_pos.pl
@@ -2,20 +2,31 @@
 %% mtx_relative_pos( +N, +K, +Hdr, +Nadj, -Pos ).
 %
 % R is the absolute position of the Nth position relative to K.
-% If N is compound it is assumed to be the RHS of an addition which
+%
+% If N unifies to abs_pos(R), then Pos is equal to R.
+% In any other case, N is assumed to be the RHS of an addition which
 % is evaluated- LHS is K. If relative position evalutes to a negative
-% is assumed to mean right relative postion in Hdr and thus 
-% mapped to the absolute position of that. In this case Nadj is also 
-% added. This provides a convenient method for referring to 
+% is assumed to mean relative position in Hdr starting from the last argument as 1.
+% mapped to the absolute position of that. 
+% In this case Nadj (negative adjustment) is also % added. 
+% This provides a convenient method for referring to 
 % negative location of transformed (relative to Hdr) matrices.
 %
 %==
 % ?- mtx_relative_pos( 2, 2, _, Pos ).
 % Pos = 4.
+% 
 % ?- mtx_relative_pos( -2, 0, c(a,b,c), Pos ).
 % Pos = 2.
-% ?- mtx_relative_pos( -2, 0, c(a,b,c), Nadj, Pos ).
+%
+% ?- mtx_relative_pos( -3, 0, c(a,b,c), Pos ).
+% Pos = 1.
+%
+% ?- mtx_relative_pos( -2, 0, c(a,b,c), 0, Pos ).
 % Pos = 2.
+% 
+% ?- mtx_relative_pos( -2, 0, c(a,b,c), 1, Pos ).
+% Pos = 3.
 %==
 % @version  0.1 2014/9/22
 %
