203e8a5549a200bdb42600070c5658c087d8c909
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-03-18 00:16:50 +0000

    ADDED:  mtx_column_threshold/3, with options

--- src/mtx_column_threshold.pl
+++ src/mtx_column_threshold.pl
@@ -3,18 +3,25 @@
 
 :- lib(compare_multi_ops_list/2).
 
-%% mtx_column_threshold( +Csv, +Clm, +Val, +Dir, -Out ).
+%% mtx_column_threshold( +Csv, +Clm, +Val, +Dir, -Sel ).
 %% mtx_column_threshold( +Csv, +Clm, +Val, +Dir, -Sel, -Rej ).
+%% mtx_column_threshold( +Csv, -Sel, +Opts ).
 %
 %  Cuts rows off Csv by thresholding Clm over or below Val according to Dir.
-%  The resulting csv is in Out.
+%  The resulting csv is in Sel.
 %  When Dir is < predicate keeps values below the threshold and Dir > keeps values
 %  (strictly) above the threshold. Note that the Clm_th value of each row 
 %  is also tested for being numeric. This makes sure we dont test against 
-%  non-numerics. Rows that have non-numeric Clm_th values are not in Out.
+%  non-numerics. Rows that have non-numeric Clm_th values are not in Sel.
 %  Clm is fed through mtx_header_column_pos/3 and can be an argument of
 %  the first term in Csv (the header) or the corresponding number. 
-%  Csv and Out are passed through mtx/2, so the can be list of row terms or csv filenames.
+%  Csv and Sel are passed through mtx/2, so the can be list of row terms or csv filenames.
+%
+%  The Opts version, only fires if the following Option is present:
+%  * mtx_cuttoff(Clm,Val,Dir)
+%    with the same semantics as the /5 version.
+%
+%  If the option does not exist, the predicate succeeds with =|Sel = Csv|=.
 %
 %  The predicate assumes Csv is of the form [Hdr|Rows] and includes Hdr to result.
 %  If you want to call on non headers Rows then with numeric NumClm you can call:
@@ -22,7 +29,7 @@
 %  ?- mtx_column_threshold( [_|Rows], NumClm, Val, Dir, [_|OutRows] ).
 %  ==
 %
-%  Exaamples
+%  Examples
 %  ==
 %  ?- assert( csv([row(a,b,c),row(1,2,3),row(1,4,5),row(3,6,7),row('',8,9),row(3,b,10)]) ).
 %  ?- csv( Csv ), mtx_column_threshold( Csv, a, 2, <, Out ).
@@ -51,6 +58,15 @@ mtx_column_threshold( CsvIn, Clm, Val, Dir, Sel, Rej ) :-
 	partition( row_threshold(Pos,Val,Dirs), Rows, SelRows, RejRows ),
 	mtx( Sel, [Hdr|SelRows] ),
 	mtx( Rej, [Hdr|RejRows] ).
+
+mtx_column_threshold( CsvIn, Sel, OptS ) :-
+     en_list( OptS, Opts ),
+     % fixme: add a way to return the Rej-ected rows
+     ( memberchk(mtx_cutoff(Clm,Val,Dir), Opts) -> 
+          mtx_column_threshold( CsvIn, Clm, Val, Dir Sel )
+          ;
+          CsvIn = Sel
+     ).
 
 row_threshold( Pos, Val, Dirs, Row ) :-
 	arg( Pos, Row, This ),
